b1c42df365c97a40b3f42f765e884aebac9e7319
Joseph Adedeji <adedejiosvaldo@gmail.com>
2024-02-21 18:35:56 +0100

    Class-Comment

--- src/class/class.service.ts
+++ src/class/class.service.ts
@@ -2,12 +2,16 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClassRepositoryInterface } from './repository/class.interface.repo';
 
 @Injectable()
+// Create Interface  that extends the BaseRepo with the entity
+// Create a repo using @InjectRepo and make it the super
+// Using {provide,useClass}, we create a value that when used, calls the repo
+// In the user class ,we inject it
 export class ClassService {
   constructor(
     @Inject('ClassRepositoryInterface')
     private readonly classRepo: ClassRepositoryInterface,
   ) {}
   async hello() {
-    this.classRepo.create
+    this.classRepo.create;
   }
 }
